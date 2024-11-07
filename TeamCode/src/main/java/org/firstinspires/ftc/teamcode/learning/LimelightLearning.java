fb1f4581b626ad71fe960075c0d0bfab6d2f5e98
Ranveer1010 <ranveer.nagar2011@gmail.com>
2024-11-07 15:46:38 -0800

    Made motor encoder manager

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learning/LimelightLearning.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.learning;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+public class LimelightLearning extends LinearOpMode {
+
+    private Limelight3A limelight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(0);
+        limelight.start();
+
+        while(opModeIsActive()) {
+
+            LLResult result = limelight.getLatestResult();
+
+            if(result != null && result.isValid()) {
+                Pose3D botpose = result.getBotpose();
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("Botpose", botpose.toString());
+
+                //Keeps getting results while opMode is active
+
+
+            }
+
+
+        }
+
+    }
+}
