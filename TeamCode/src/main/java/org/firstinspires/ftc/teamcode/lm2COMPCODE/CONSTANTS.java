ac1d311b8c587363c74509a6ce79871b52434215
Parshwa <parshwashah282011@gmail.com>
2024-11-18 15:07:27 -0800

    NEW STUFF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lm2COMPCODE/CONSTANTS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lm2COMPCODE/CONSTANTS.java
@@ -7,7 +7,11 @@ public class CONSTANTS {
     public static final double SERVOROTATEHIGH = 0.6;
     public static final double SERVOOPEN = 0.4;
     public static final double SERVOCLOSE = 0.7;
+    public static final double SERVOROTATE2LEFT90 = 0.5;
+    public static final double SERVOROTATE2LEFT45 = 0.5;
     public static final double SERVOROTATE2MID = 0.5;
+    public static final double SERVOROTATE2RIGHT45 = 0.5;
+    public static final double SERVOROTATE2RIGHT90 = 0.5;
 
     public static final int SLIDEEXPANSTIONMAX = 0;
     public static final int SLIDEEXPANTIONLOW = 0;
