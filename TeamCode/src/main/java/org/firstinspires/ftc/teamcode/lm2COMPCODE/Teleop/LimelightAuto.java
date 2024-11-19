ed922ef45f7a8d85290f2cc2480be15d4e848720
Ranveer1010 <ranveer.nagar2011@gmail.com>
2024-11-18 19:22:08 -0800

    Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lm2COMPCODE/Teleop/LimelightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lm2COMPCODE/Teleop/LimelightAuto.java
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.lm2COMPCODE.AUTONOMOUS;
+package org.firstinspires.ftc.teamcode.lm2COMPCODE.Teleop;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.hardware.limelightvision.LLStatus;
-import com.qualcomm.hardware.limelightvision.LLFieldMap;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name = "LimelightTest")
 
 public class LimelightAuto extends LinearOpMode
 {
