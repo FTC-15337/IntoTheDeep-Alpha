8a09a49dc332d7b00304939897fa665e0f1eae2b
aarav <aarav2win@gmail.com>
2024-12-15 16:01:26 -0800

    Breaking up jobs for right auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Interleauge/NewRightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Interleauge/NewRightAuto.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.Interleauge;
 
-public class NewRightAuto {
+public class NewRightAuto
+{
+import static com.qualcomm.robotcore.util.ElapsedTime.Resolution.MILLISECONDS;
+
+import com.parshwa.drive.auto.AutoDriverBetaV1;
+import com.parshwa.drive.tele.Drive;
+import com.parshwa.drive.tele.DriveModes;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.lm2COMPCODE.AUTONOMOUS.Threads.Lights;
+import org.firstinspires.ftc.teamcode.lm2COMPCODE.AUTONOMOUS.packages.SliderManger;
+import org.firstinspires.ftc.teamcode.lm2COMPCODE.AUTONOMOUS.packages.servoManger;
 }
+
+
