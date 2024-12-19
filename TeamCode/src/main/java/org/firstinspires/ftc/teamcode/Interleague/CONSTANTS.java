d004ffce49b9456b6862b0f57d9e9392638b0062
Ranveer1010 <ranveer.nagar2011@gmail.com>
2024-12-18 18:55:49 -0800

    Fixing claw values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Interleague/CONSTANTS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Interleague/CONSTANTS.java
@@ -9,13 +9,13 @@ public class CONSTANTS {
         public static double SERVOROTATELOWESTDELETE = 0.05;
         public static double SERVOROTATELOWEST = 1.0;
         public static double SERVOROTATELOW = 0.9;
-        public static double SERVOROTATEMIDDLE = 0.0;
+        public static double SERVOROTATEMIDDLE = 0.6;
         public static double SERVOROTATEHIGH = 0.4;
         public static double SERVOOPEN = 1.0;
         public static double SERVOCLOSE = 0.7;
         public static double SERVOROTATE2LEFT90 = 0.5;
         public static double SERVOROTATE2LEFT45 = 0.5;
-        public static double SERVOROTATE2MID = 0.7;
+        public static double SERVOROTATE2MID = 0.2;
         public static double SERVOROTATE2RIGHT45 = 0.5;
         public static double SERVOROTATE2RIGHT90 = 0.5;
         public static double CLAWSTARTPOS = 0.7;
